0eed2c0cb95c93097eec9f0640acb8f9f063d971
AliAtherAyyubi <info2atherayyubi@gmail.com>
2025-07-04 22:29:42 +0500

    feat: add components

---
+++ src/components/QuoteForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { AuroraText } from './magicui/aurora-text'
+
+export const QuoteForm = () => {
+  return (
+    <>
+    <div className="flex justify-center items-center flex-col my-10">
+      <h1 className="text-4xl font-bold tracking-tighter md:text-5xl lg:text-7xl">
+       Quote <AuroraText> Generator</AuroraText>
+    </h1>
+      
+    </div>
+    </>
+  )
+}
