6051f76b8482924b258e2ebbef11e6c61e6f86d7
AliAtherAyyubi <info2atherayyubi@gmail.com>
2025-07-06 21:57:51 +0500

    feat: gradients

---
+++ src/lib/gradients.ts
@@ -0,0 +1,40 @@
+
+export const gradients = [
+  // Purple & Indigo
+  'bg-[linear-gradient(135deg,_#5a67d8_0%,_#6b46c1_100%)]',
+
+  // Pink & Red
+  'bg-[linear-gradient(135deg,_#e64980_0%,_#d6336c_100%)]',
+
+  // Sky Blue & Cyan
+  'bg-[linear-gradient(135deg,_#3b82f6_0%,_#06b6d4_100%)]',
+
+  // Emerald & Teal
+  'bg-[linear-gradient(135deg,_#10b981_0%,_#14b8a6_100%)]',
+
+  // Deep Pink & Amber
+  'bg-[linear-gradient(135deg,_#ec4899_0%,_#f59e0b_100%)]',
+
+  // Blue Gray & Pink
+  'bg-[linear-gradient(135deg,_#4b5563_0%,_#f472b6_100%)]',
+
+  // Orange & Rose
+  'bg-[linear-gradient(135deg,_#f97316_0%,_#fb7185_100%)]',
+
+  // Warm Yellow to Red
+  'bg-[linear-gradient(135deg,_#facc15_0%,_#ef4444_100%)]',
+
+  // Indigo to Violet
+  'bg-[linear-gradient(135deg,_#6366f1_0%,_#8b5cf6_100%)]',
+
+  // Teal to Blue
+  'bg-[linear-gradient(135deg,_#0d9488_0%,_#2563eb_100%)]',
+   // Teal to Lime
+  'bg-[linear-gradient(135deg,_#00b09b_0%,_#96c93d_100%)]',
+
+  // Purple to Pink
+  'bg-[linear-gradient(135deg,_#9796f0_0%,_#fbc7d4_100%)]',
+
+  // Light Blue to Deep Blue
+  'bg-[linear-gradient(135deg,_#a8c0ff_0%,_#3f2b96_100%)]'
+];
